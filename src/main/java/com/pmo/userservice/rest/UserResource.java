5b901f2c2215a9758dc686640e819729e1f2c8cb
ashehzad <muhammad.shahzad@backcountry.com>
2022-11-22 12:07:32 +0500

    refactor package name

---
+++ src/main/java/com/pmo/userservice/rest/UserResource.java
@@ -0,0 +1,12 @@
+package com.pmo.userservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/user")
+public class UserResource {
+
+
+}
---
+++ src/main/java/com/pmo/userservice/service/UserService.java
@@ -0,0 +1,11 @@
+package com.pmo.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service class for managing users.
+ */
+@Service
+public class UserService {
+
+}
